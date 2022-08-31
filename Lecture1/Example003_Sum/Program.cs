91764546ed5bfd63d9329bee5b60bdb3a2ce0ef4
Druce <raa-07@mail.ru>
2022-08-31 23:14:57 +0300

    Lec2

---
+++ Lecture1/Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numderA = new Random().Next(1,10); // 1 2 3 /... 9
+Console.WriteLine(numderA);
+int numderB = new Random().Next(1,10); // 1 2 3 /... 9
+Console.WriteLine(numderB);
+int result = numderA + numderB;
+Console.WriteLine(result);
